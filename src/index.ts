b578a20f2c9508314ecb41ebf7250ce5db2ba2b8
Jean-Baptiste Terrazzoni <j2aa@pm.me>
2024-05-11 22:26:30 +0200

    feat: export eslint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import jestConfig from './jest/main.js';
+import jestEslint from './jest-eslint/main.js';
 import mock from './jest-mock-extended/main.js';
 import supertest from './supertest/main.js';
 
-export { jestConfig, mock, supertest };
+export { jestConfig, mock, supertest, jestEslint };
